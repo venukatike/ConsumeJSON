b43a3ccf64278c80c4cfa28bffd40e8b97694d44
Venu <Venu@DESKTOP-PADD8DL>
2023-05-15 23:43:56 +0530

    Add project files.

---
+++ ConsumeJSON/ConsumeJSON/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ConsumeJSON.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ConsumeJSON
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<TestModel> myobj;
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("ConsumeJSON.testmodels.json");
+
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                var json = reader.ReadToEnd();
+
+                List<TestModel> mylist = JsonConvert.DeserializeObject<List<TestModel>>(json);
+                myobj = new ObservableCollection<TestModel>(mylist);
+                MyListView.ItemsSource = myobj;
+            }
+
+        }
+    }
+}
---
+++ ConsumeJSON/ConsumeJSON/Models/TestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsumeJSON.Models
+{
+    public class TestModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
